a60cdb7b29ad78f336bddb1de33a25bea7b86982
sybillereuter <sybille.reuter.berlin@gmail.com>
2024-11-17 14:23:03 +0100

    v1.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='pyteonline',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=install_requires,
