5d0e87ffe1dc5b075be109a4db9595005bfe109d
sybillereuter <sybille.reuter.berlin@gmail.com>
2025-04-19 22:15:43 +0200

    v1.1.3: security update for torch

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='pyteonline',
-    version='1.1.2',
+    version='1.1.3',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=install_requires,
